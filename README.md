518a79b0b3e4ac8c19e03b7865282116fad093b6
1C0D <1conscience@gmx.fr>
2023-12-18 14:43:13 +0100

    settings color size

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Vault Name in Status Bar  
 
-Add your vault name to the status bar.
-Can be reduced to :: by cliking on it  
-Option setting to have it reduced at start or not  
-N.B: You can change the style in the css file
+New: change color and size in settings
 
+Add your vault name to the status bar.
+Can be reduced to :: by clik 
 ![img1](./enlarged.jpg)  ![img2](./reduced.jpg)  
 
+## Settings
+- Option to reduce the title in StatusBar at start
+- Set color and size
+
+
+## Demo 
 ![demo](NameVaultStatusBar.gif)
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { App, Plugin, PluginSettingTab, Setting, View } from "obsidian";
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface SBVNSettings {
 	reducedAtStart: boolean;
+	color: string;
+	fontSize: number
 }
 
 const DEFAULT_SETTINGS: SBVNSettings = {
 	reducedAtStart: false,
+	color: "#424cd8",
+	fontSize: 1.5
 };
 
 export default class StatusBarVaultName extends Plugin {
@@ -15,13 +19,14 @@ export default class StatusBarVaultName extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new Settings(this.app, this));
-		const { workspace } = this.app;
 		const vaultName = this.app.vault.getName();
 		const statusBar = document.querySelector('.status-bar')
 		this.title = document.createElement('div');
 		this.settings.reducedAtStart ? this.title.innerText = "::" : this.title.innerText = vaultName
 		this.title.classList.add("status-bar-vault-name");
 		statusBar?.prepend(this.title)
+		this.title.style.color = this.settings.color;
+		this.title.style.fontSize = `${this.settings.fontSize}em`;
 
 		this.title.addEventListener('click', () => {
 			if (this.title.innerText === vaultName) {
@@ -81,6 +86,28 @@ class Settings extends PluginSettingTab {
 							: (this.plugin.title.innerText = this.app.vault.getName());
 					});
 			});
+		const setting = new Setting(containerEl)
+			.setName("Set color & size")
+			.addColorPicker(color => color
+				.setValue(this.plugin.settings.color)
+				.onChange(async (value) => {
+					this.plugin.settings.color = value;
+					this.plugin.title.style.color = value;
+					this.plugin.saveSettings();
+				})
+			)
+		setting
+			.addSlider((slider) => {
+				slider
+					.setLimits(1, 3, 0.1)
+					.setValue(this.plugin.settings.fontSize)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.fontSize = value;
+						this.plugin.title.style.fontSize = `${value}em`;
+						await this.plugin.saveSettings();
+					});
+			})
 	}
 }
 
